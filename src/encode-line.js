7b8e0c0d71aaae1db8e80cb85bc46bfbc0010053
YuliaLugovaya <yulialugovayaphoto@gmail.com>
2023-04-16 01:56:08 +0300

    feat: add solutions for encode-line, get-email, mac-address

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let code = '';
+  let count = 1;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count += 1;
+    } else {
+      if (count === 1) {
+        code += str[i];
+      } else {
+        code += count + str[i];
+        count = 1;
+      }
+    }
+  }
+
+  return code;
 }
 
 module.exports = {
